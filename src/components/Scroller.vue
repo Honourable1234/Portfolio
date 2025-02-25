e2dd5fcf1857a94870a23b53443f63963c4a465d
omobolaji <omobolaji119@gmail.com>
2025-02-25 12:28:16 +0100

    commit for 25 february 2025

--- src/components/Scroller.vue
+++ src/components/Scroller.vue
@@ -13,31 +13,31 @@
         <div
           v-for="(section, index) in sections"
           :key="index"
-          class="relative flex-shrink-0 flex flex-col items-center justify-center rounded-lg shadow-lg p-6 transition-all duration-500 w-full h-[350px] overflow-hidden"
+          class="relative flex-shrink-0 flex flex-col rounded-lg shadow-lg p-6 transition-all duration-500 w-full h-[350px] overflow-hidden"
         >
           <!-- Background Image (Absolute Positioning) -->
           <img
             :src="section.image"
             alt="Section Image"
-            class="absolute top-0 left-0 w-full h-full object-cover z-0  rounded-[20px]"
+            class="absolute top-0 left-0 w-full h-full object-cover z-0 opacity-50  rounded-[20px]"
           />
   
           <!-- Text Content (Above Image) -->
-          <div class="relative z-10 text-left py-[20px]">
-            <h2 class="text-3xl font-bold" :style="{ color: section.textColor }">
+          <div class="relative z-10 text-center">
+            <h2 class="text-[24px] font-black drop-shadow-lg" :style="{ color: section.textColor }">
               {{ section.title }}
             </h2>
-            <p class="text-lg mt-2" :style="{ color: section.textColor }">
+            <p class="text-[18px] mt-2 font-extrabold drop-shadow-lg" :style="{ color: section.textColor }">
               {{ section.description }}
             </p>
-            <P class="text-[15px] mt-2" :style="{ color: section.textColor }">Check out this project at <a>{{ section.link }}</a></P>
+            <P class="text-[15px] mt-2 font-bold drop-shadow-lg" :style="{ color: section.textColor }">Click  <a :href="section.link" target="_blank" rel="noopener" class="underline text-blue-500">here</a> to check it out</P>
           </div>
         </div>
       </div>
   
       <!-- Left Button -->
       <button
-        class="absolute left-0 top-1/2 transform -translate-y-1/2 bg-gray-300 opacity-[40%] w-[47px] h-[47px] rounded-full flex items-center justify-center shadow-lg z-10"
+        class="absolute left-0 top-3/4 transform -translate-y-1/2 bg-gray-300 opacity-[70%] w-[47px] h-[47px] rounded-full flex items-center justify-center shadow-lg z-10"
         @click="scroll('left')"
       >
         ⬅️
@@ -45,7 +45,7 @@
   
       <!-- Right Button -->
       <button
-        class="absolute right-0 top-1/2 transform -translate-y-1/2 bg-gray-300 opacity-[40%] w-[47px] h-[47px] rounded-full flex items-center justify-center shadow-lg z-10"
+        class="absolute right-0 top-3/4 transform -translate-y-1/2 bg-gray-300 opacity-[70%] w-[47px] h-[47px] rounded-full flex items-center justify-center shadow-lg z-10"
         @click="scroll('right')"
       >
         ➡️
@@ -55,7 +55,6 @@
   
   <script setup lang="ts">
   import { ref, onMounted, onUnmounted } from "vue";
-  import gitHubLogo from "@/assets/gitHub.svg"
   
   // Define Type for Sections
   interface Section {
@@ -63,6 +62,7 @@
     description: string;
     image: string;
     textColor: string;
+    link: string;
   }
   
   // References
@@ -74,25 +74,55 @@
   const sectionWidth = ref<number>(0);
   
   // Sections Data
-  const sections = ref<section[]>([
+  const sections = ref<Section[]>([
     {
-      title: "Vue.js",
-      description: "A progressive JavaScript framework for building UI.",
-      image: "https://imgur.com/a/nXr7tCV.jpg",
+      title: "Study Hug",
+      description: "A web app to practice CBT exams for UTME students",
+      image: "https://res.cloudinary.com/dabihkozn/image/upload/v1740439198/dkp0pumiplvgfbbet1wx.png",
+      textColor: "yellow-400",
+      link: "https://studyhug.netlify.app/"
+    },
+    {
+      title: "Track IT",
+      description: "Demo financial tracking app built with Chakra UI",
+      image: "https://res.cloudinary.com/dabihkozn/image/upload/v1740479250/Screenshot_2025-02-25_112655_qinfjt.png",
+      textColor: "black",
+      link: "https://tractit.netlify.app/"
+    },
+    {
+      title: "Pay-4-me",
+      description: "An informative landing page for a payment app",
+      image: "https://res.cloudinary.com/dabihkozn/image/upload/v1740479898/Screenshot_2025-02-25_113740_fgjxqz.png",
+      textColor: "black",
+      link: "https://pay-4-me.netlify.app/"
+    },
+    {
+      title: "My stock Tracker",
+      description: "Displays stock and crypto prices from an API",
+      image: "https://res.cloudinary.com/dabihkozn/image/upload/v1740480369/Screenshot_2025-02-25_114540_pcijtd.png",
       textColor: "#FFFFFF",
-      link: ""
+      link: "https://mystocktrackerr.netlify.app/"
     },
     {
-      title: "React.js",
-      description: "A library for building user interfaces with components.",
-      image: "/images/react-bg.jpg",
-      textColor: "#FFDD00",
+      title: "Wabais",
+      description: "A landing page for a branding business",
+      image: "https://res.cloudinary.com/dabihkozn/image/upload/v1740480711/Screenshot_2025-02-25_115127_oosifp.png",
+      textColor: "#32CD32",
+      link: "https://wabais.netlify.app/"
     },
     {
-      title: "Angular",
-      description: "A powerful framework for building enterprise-grade apps.",
-      image: "/images/angular-bg.jpg",
-      textColor: "#FF5733",
+      title: "Nexcent",
+      description: "A landing page for a branding business",
+      image: "https://res.cloudinary.com/dabihkozn/image/upload/v1740481008/Screenshot_2025-02-25_115623_i3qahh.png",
+      textColor: "black",
+      link: "https://nexcentbyhonourable.netlify.app/"
+    },
+    {
+      title: "Biccas",
+      description: "A landing page for a Virtual assistance business",
+      image: "https://res.cloudinary.com/dabihkozn/image/upload/v1740481452/Screenshot_2025-02-25_120140_vpiwyk.png",
+      textColor: "black",
+      link: " https://biccaspage.netlify.app/"
     },
   ]);
   
